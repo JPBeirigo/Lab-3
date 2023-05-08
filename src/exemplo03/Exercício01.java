ecf204917e29d8cfcf6f4fc778cc946c212c0c60
Grannek <124587753+Grannek@users.noreply.github.com>
2023-05-08 10:14:00 -0300

    Aula 08/05
    Aula do dia.
    
    Tivemos introdução a interfaces gráficas mas antes disso corrigi o código de um exercício antigo.

--- src/exemplo03/Exercício01.java
+++ src/exemplo03/Exercício01.java
@@ -10,31 +10,41 @@ import javax.swing.JOptionPane;
 public class Exercício01 {
     public static void main(String[] args) {
         
-        String str1 = JOptionPane.showInputDialog(
-                "Insira o nome do produto: ");
-        String str2 = JOptionPane.showInputDialog(
-                "Insira o valor do produto: ");
-       
-        double preco = double.parsedouble(str2);
+        String nome = JOptionPane.showInputDialog(
+            "Nome do produto"
+        );
+        double valor = Double.parseDouble(
+            JOptionPane.showInputDialog("Valor do produto")
+        );
+        double desconto = 0.0;
         
-        if (preco>=50 && <200)
-        {
-            int desconto = 5;}
-        else if (preco>=200 && < 500){
-            int desconto = 6;
+        if (valor < 0){
+            JOptionPane.showMessageDialog(
+                null, 
+                "O valor informado deve ser maior que zero"
+            );
+        }else{
+            if (valor >= 50 && valor < 200){
+                desconto = 0.05; // 5%
+            }else if (valor >=200 & valor < 500){
+                desconto = 0.06;
+            }else if (valor >=500 & valor < 1000){
+                desconto = 0.07;
+            }else if (valor >= 100){
+                desconto = 0.08;
             }
-        else if (preco>=500 && <1000){
-            int desconto = 7;
-            }
-        else if (preco<0){
-                JOptionPane.showMessageDialog(
-            null,
-            "Demonstração do uso da caixa de diálogo na Linguagem Java",
-            "Lab.Programação III",
-            JOptionPane.WARNING_MESSAGE
-       );
-                }
-        
-    }
-    
+            
+            JOptionPane.showMessageDialog(
+                null,
+                String.format(
+                    """
+                        Produto: %s
+                        Valor: R$ %.2f 
+                        Valor com Desconto R$ %.2f
+                    """,
+                    nome, valor, (valor-(valor*desconto))
+                )
+            );
+        }
+    }    
 }
